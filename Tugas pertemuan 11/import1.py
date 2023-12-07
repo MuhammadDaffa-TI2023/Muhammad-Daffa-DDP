b0490393fd5ae93c84127f324ab0d571c2ad873a
MuhammadDaffa-TI2023 <daffaaja530@gmail.com>
2023-12-07 19:23:32 +0700

    ok

---
+++ Tugas pertemuan 11/import1.py
@@ -0,0 +1,9 @@
+from soal1 import *
+
+
+
+persegi_panjang(4, 2)
+
+jajar_genjang(5, 3, 4)
+
+persegi(2)
---
+++ module/belajar1.py
@@ -0,0 +1,16 @@
+import sys
+import string
+import math
+import datetime
+
+
+print(sys.copyright)
+
+
+print(string.ascii_lowercase)
+
+print(math.pow(2,3))
+
+print(datetime.datetime.now())
+
+
